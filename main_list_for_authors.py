17c20a7d97c4954b8b5f89ea5e4c7eb48b5dd384
alex_kondr <alex_kondr@outlook.com>
2023-02-26 21:34:05 +0200

    save

--- main_list_for_authors.py
+++ main_list_for_authors.py
@@ -1,50 +1,31 @@
+import json
+
 import scrapy
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 
-class AuthorsSpider(scrapy.Spider):
-    name = "list_authors"
+class FindUrlsForAuthors(scrapy.Spider):
+    name = "find_urls_for_authors"
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["http://quotes.toscrape.com"]
+    new_start_urls = set()
     custom_settings = {
         "FEED_FORMAT": "json", 
         "FEED_URI": "list_authors.json"
     }
     
     def parse(self, response):
-        
-        # authors = response.xpath("/html//small[@class='author']/text()").extract()
-        # yield authors
         links = response.xpath("//div[@class='quote']/span/a/@href").extract()
-        # print(f"{links=}")
-        # for link in links:
-        # if links:
-        #     yield scrapy.Request(url=self.start_urls[0]+links)
-        # self.start_urls = []
         
-        # for link in links:
-        #     self.start_urls.append("http://quotes.toscrape.com"+link)
-            # yield scrapy.Request(url=self.start_urls[0]+link)
-            # scrapy.Request(url=self.start_urls[0]+link)
-        # print("author: ", response.xpath("//h3[@class='author-title']/text()").get())
-        # for author in response.xpath("/html//div[@class='author-details']"):
-                
-        
-        # for link_ in self.start_urls:
-        #     yield scrapy.Request(url=link_)
-            
-            # print(f"{response=}")
-            
-        # author =  response.xpath("//h3[@class='author-title']/text()").get()
-        
-        # if author:
-            
-        yield {
-            # "tags": quote.xpath("div[@class='tags']/a/text()").extract(),
-            "links": links
-            # "author": self.start_urls[0]+link
-            # "quote": quote.xpath("span[@class='text']/text()").get()
-        }
+        for link in links:
+            self.new_start_urls.add()
+        # yield {
+        #     # "tags": quote.xpath("div[@class='tags']/a/text()").extract(),
+        #     "links": links
+        #     # "author": self.start_urls[0]+link
+        #     # "quote": quote.xpath("span[@class='text']/text()").get()
+        # }
         
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         
@@ -53,6 +34,55 @@ class AuthorsSpider(scrapy.Spider):
 # print(authors)
 
 
-process = CrawlerProcess()
+process = CrawlerProcess(settings=get_project_settings())
+process.crawl(FindUrlsForAuthors)
+process.start()
+process.stop()
+
+print("--------------------")
+print("End spyder")
+
+# import list_authors
+with open("list_authors.json", "r") as file:
+    list_authors = json.load(file)
+
+list_links = []
+
+for list_author in list_authors:
+    list_links += list_author.get("links")
+    
+list_links1 = set(list_links)
+start_urls = []
+
+for list_ in list_links1:
+    start_urls.append("http://quotes.toscrape.com"+list_)
+    
+    
+class AuthorsSpider(scrapy.Spider):
+    name = "list_authors"
+    allowed_domains = ["quotes.toscrape.com"]
+    start_urls = start_urls
+    custom_settings = {
+        "FEED_FORMAT": "json", 
+        "FEED_URI": "authors_spider.json"
+    }
+    
+    def parse(self, response):
+            
+        yield {
+            # "tags": quote.xpath("div[@class='tags']/a/text()").extract(),
+            "author": response.xpath("//h3[@class='author-title']/text()").get()
+            # "author": self.start_urls[0]+link
+            # "quote": quote.xpath("span[@class='text']/text()").get()
+        }
+        
+        # next_link = response.xpath("//li[@class='next']/a/@href").get()
+        
+        # if next_link:
+        #     yield scrapy.Request(url=self.start_urls[0]+next_link)
+# print(authors)
+
+
+# process1 = CrawlerProcess()
 process.crawl(AuthorsSpider)
 process.start()
